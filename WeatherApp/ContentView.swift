f246e711f12baa12a3d63a6c007e896cd4f5da06
Shehryar M <shehryarm1029@gmail.com>
2025-12-17 20:01:34 -0500

    visual fix

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -16,78 +16,80 @@ extension Font {
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                
-            }
-            HStack {
-                Text("Oakville")
-                    .font(.nunitoTitle)
-                    .fontWeight(.bold)
-            }
-        }
-        
-        HStack {
-            VStack(alignment: .trailing, spacing: 20) { // spacing useful for gaps in stacks
-                Image(systemName: "cloud.moon.circle.fill")
-                    .resizable()
-                    .frame(width: 60, height: 60)  // set specific dimensions
-                VStack(alignment: .leading, spacing: -5) {
-                    Text("44°")
-                        .font(.nunitoTitle)
-                        .fontWeight(.semibold)
-                    Text("Feels 41°")
-                        .font(.nunitoSmall)
-                        .fontWeight(.semibold)
-                }
-                .padding(.trailing, 30)
-                
-            }
-            .frame(width: 150, height: 180)
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 30))
-            .foregroundStyle(Color.white) // text color
-
             VStack {
-                HStack (spacing: 8) {
-                    Image(systemName: "cloud.rain.fill")
-                        .resizable()
-                        .frame(width: 25, height: 25)
-                    Text("5%")
-                        .font(.nunitoReg)
-                        .fontWeight(.semibold)
+                HStack {
+                    
                 }
-                .frame(width: 150, height: 85)
-                .background(Color.yellow)
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .foregroundStyle(Color.black)
-
-                VStack (spacing: 0) {
-                    HStack{
-                        Image(systemName: "arrow.up")
-                            .resizable()
-                            .frame(width: 15, height: 15)
-                            .fontWeight(.bold)
-                        Text("58°")
-                            .font(.nunitoReg)
-                            .fontWeight(.semibold)
-                    }
-                    HStack{
-                        Image(systemName: "arrow.down")
-                            .resizable()
-                            .frame(width: 15, height: 15)
-                            .fontWeight(.bold)
-                        Text("41°")
-                            .font(.nunitoReg)
-                            .fontWeight(.semibold)
-                    }
+                HStack {
+                    Text("Oakville")
+                        .font(.nunitoTitle)
+                        .fontWeight(.bold)
                 }
-                .frame(width: 150, height: 85)
-                .background(Color.green)
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .foregroundStyle(Color.white)
-
             }
-            
+        
+            HStack {
+                VStack(alignment: .trailing, spacing: 20) { // spacing useful for gaps in stacks
+                    Image(systemName: "cloud.moon.circle.fill")
+                        .resizable()
+                        .frame(width: 60, height: 60)  // set specific dimensions
+                    VStack(alignment: .leading, spacing: -5) {
+                        Text("24°")
+                            .font(.nunitoTitle)
+                            .fontWeight(.semibold)
+                        Text("Feels 21°")
+                            .font(.nunitoSmall)
+                            .fontWeight(.semibold)
+                    }
+                    .padding(.trailing, 30)
+                    
+                }
+                .frame(width: 150, height: 180)
+                .background(Color.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .foregroundStyle(Color.white) // text color
+
+                VStack {
+                    HStack (spacing: 8) {
+                        Image(systemName: "cloud.rain.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                        Text("5%")
+                            .font(.nunitoReg)
+                            .fontWeight(.semibold)
+                    }
+                    .frame(width: 150, height: 85)
+                    .background(Color.yellow)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    .foregroundStyle(Color.black)
+
+                    VStack (spacing: 0) {
+                        HStack{
+                            Image(systemName: "arrow.up")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .fontWeight(.bold)
+                            Text("28°")
+                                .font(.nunitoReg)
+                                .fontWeight(.semibold)
+                        }
+                        HStack{
+                            Image(systemName: "arrow.down")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .fontWeight(.bold)
+                            Text("21°")
+                                .font(.nunitoReg)
+                                .fontWeight(.semibold)
+                        }
+                    }
+                    .frame(width: 150, height: 85)
+                    .background(Color.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    .foregroundStyle(Color.white)
+
+                }
+                
+            }
         }
     }
 }
